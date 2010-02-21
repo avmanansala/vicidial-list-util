cf9d7c115d6a4e9e3de9f1de66a25c93706540eb
Lito Manansala <avmanansala@gmail.com>
2010-02-21 12:37:35 +0800

    Added  ADMIN_scrub_list.rb

---
+++ ADMIN_scrub_list.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/ruby
+#
+#ADMIN_scrub_list.rb
+#Description: Scrub Vicidial Call List from dnc database
+#License: Angelito Manasala avmanansala@gmail.com Licese GPL v2
+
+
+puts "ADMIN_scrub_list.rb"
