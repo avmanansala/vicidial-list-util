3c7368abecac0eaccb98f07bed5bfa0fcc95cde1
Lito Manansala <avmanansala@gmail.com>
2010-02-21 13:03:08 +0800

    Added db_conn.rb and seperated mysql connection block from Admin_scrub_list.rb

--- ADMIN_scrub_list.rb
+++ ADMIN_scrub_list.rb
@@ -9,22 +9,14 @@ require ('parseconfig')
 require ('active_record')
 require ('active_support')
  
-#Parse astguiclient configuration file.
-my_config = ParseConfig.new('/etc/astguiclient.conf')
-mysql_server_ip = my_config.get_value('VARserver_ip').gsub('>','').strip
-mysql_server = my_config.get_value('VARDB_server').gsub('>','').strip
-mysql_database = my_config.get_value('VARDB_database').gsub('>','').strip
-mysql_user = my_config.get_value('VARDB_user').gsub('>','').strip
-mysql_pass = my_config.get_value('VARDB_pass').gsub('>','').strip
-mysql_port = my_config.get_value('VARDB_port').gsub('>','').strip
-#end::Parse config
+require 'db_conn.rb'
 
 ActiveRecord::Base.establish_connection(
 :adapter => 'mysql',
-:host => mysql_server_ip,
-:database => mysql_database,
-:username => mysql_user,
-:password => mysql_pass
+:host => @mysql_server_ip,
+:database => @mysql_database,
+:username => @mysql_user,
+:password => @mysql_pass
 )
 
 
