7935e5718fc92f773529b0f7e01387593ce6da2f
Lito Manansala <avmanansala@gmail.com>
2010-02-22 13:56:06 +0800

    started for first phone_number lookup and compate to vicidial_dnc2 table

--- ADMIN_scrub_list.rb
+++ ADMIN_scrub_list.rb
@@ -10,6 +10,11 @@ require ('active_record')
 require ('active_support')
 require 'db_conn.rb'
 
+
+list_id = ARGV[0]
+
+puts "List id to scrub: #{list_id}\n"
+
 ActiveRecord::Base.establish_connection(
 :adapter => 'mysql',
 :host => @mysql_server_ip,
@@ -25,11 +30,24 @@ end
 class Vicidial_list < ActiveRecord::Base
   set_table_name "vicidial_list"
 end
+class Vicidial_dnc < ActiveRecord::Base
+  set_table_name "vicidial_dnc"
+end
+class Vicidial_dnc2 < ActiveRecord::Base
+  set_table_name "vicidial_dnc2"
+end
 
-puts 'Listing List ID'
-
-Vicidial_lists.all().each do |lists| 
-  puts "List ID: #{lists.list_id}\n"
+#iterate through selected lists
+Vicidial_list.all(:conditions => "list_id = '#{list_id}'").each do |list|
+  
+  puts "Checking #{list.phone_number}..."
+  
+  #check is record exist on dnc and dnc2 table
+  Vicidial_dnc2.all(:all, :conditions => "phone_number = '#{list.phone_number}'").each do |list| 
+    puts "#{list.phone_number} DNC matches!..."
+  end
+     
+  
 end
 
 
